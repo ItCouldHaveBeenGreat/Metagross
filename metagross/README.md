4a96c6ee35816a6e8e24e849db0f947ee7b7286a
ItCouldHaveBeenGreat <25428548+ItCouldHaveBeenGreat@users.noreply.github.com>
2025-03-05 23:16:08 -0800

    Snapshot; some things work

---
+++ metagross/README.md
@@ -0,0 +1,11 @@
+
+
+### Exporting Datasets
+NOTE: The current process sucks
+```
+cd lib/pokemon-showdown-0.11.9
+npm run build
+tsc metagross/export_moves.ts  # this will fail
+node metagross/export_moves.js
+mv moves_export.json ../../metagross/export/
+```
--- tests/test_state_from_log.py
+++ tests/test_state_from_log.py
@@ -2,21 +2,31 @@ import json
 import logging
 import pytest
 
-from metagross.state_from_log import parse_log
+from metagross.showdown_engine import ShowdownEngine
+from metagross.state_from_log import MOVE_ID_TO_MOVE, parse_log
 
 PLACEHOLDER_ITEM = "unknown_item"
 PLACEHOLDER_ABILITY = "unknown_ability"
 PLACEHOLDER_MOVE = "unknown_move"
 
+
 def test_parse_log():
-    with open('tests/test_data/sample_input2.json', 'r') as f:
+    with open("tests/test_data/sample_input2.json", "r") as f:
         log_data = json.load(f)
 
-    logs = log_data['logs']
+    logs = log_data["logs"]
     state = parse_log(logs)
+
+    logging.warning(MOVE_ID_TO_MOVE)
 
     # Define the expected state here
     expected_state = {}
-    
+
     logging.warning(f"STATE: {json.dumps(state, indent=4)}")
-    assert state == expected_state
+    state_filename = "tests/test_data/test_parse_log_output.json"
+    json.dump(state, open(state_filename, "w"), indent=4)
+    # assert state == expected_state
+
+    # the ultimate test... is instantiating a showdown simulator instance
+    simulator = ShowdownEngine()
+    simulator.initialize_from_state(state_filename)
